633d3400c445aaf7d5b43b618de99cf075774062
Baraa Bakhsh <bakhshb@gmail.com>
2014-08-09 02:29:43 +1200

    updated

---
+++ com.kt.booking.restful/src/com/kt/booking/restful/V1_customerRest.java
@@ -0,0 +1,70 @@
+package com.kt.booking.restful;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONObject;
+
+import com.kt.booking.controller.CustomerManager;
+import com.kt.booking.model.Customer;
+import com.kt.booking.util.ToJSON;
+
+@Path("/v1/customer")
+public class V1_customerRest {
+
+	@POST
+	@Consumes({MediaType.APPLICATION_FORM_URLENCODED,MediaType.APPLICATION_JSON})
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response returnAllTest(String incomingData) throws Exception {
+		String returnString = null;
+		Response rb = null;	
+		JSONArray jsonArray = new JSONArray();
+		JSONObject jsonObject = new JSONObject(incomingData);
+		
+		try{
+			JSONObject partsData = new JSONObject(incomingData);
+			CustomerManager manager = new CustomerManager();
+			Customer customer = new Customer ();
+			customer.setFirstName(partsData.optString("first_name"));
+			int http_code  =manager.addCustomer(customer);
+			if( http_code == 200 ) {
+				jsonObject.put("HTTP_CODE", "200");
+				jsonObject.put("MSG", "Item has been entered successfully, Version 3");
+				returnString = jsonArray.put(jsonObject).toString();
+			}else {
+				return Response.status(500).entity("Unable to enter Item").build();
+			}
+		}catch (Exception ex){
+			ex.printStackTrace();
+			//return Response.status(500).entity("Server was not able to process your request").build();
+		}
+		return Response.ok(returnString).build();
+	}
+	
+	@Path ("/returnlist")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response returnAllTest() throws Exception {
+		String returnString = null;
+		Response rb = null;	
+		JSONArray json = new JSONArray();
+		ToJSON converter = new ToJSON();
+		try {
+			
+			json = converter.queryReturnCustomerNames();
+			
+			returnString = json.toString();
+			rb = Response.ok(returnString).build();
+			
+		}catch (Exception ex){
+			ex.printStackTrace();
+		}
+		return rb;
+	}
+}
--- com.kt.booking.restful/src/com/kt/booking/restful/status/V1_status.java
+++ com.kt.booking.restful/src/com/kt/booking/restful/status/V1_status.java
@@ -4,7 +4,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-@Path ("v1/")
+@Path ("v1")
 public class V1_status {
 
 	private static final String api_version = "00.0.1.00";
