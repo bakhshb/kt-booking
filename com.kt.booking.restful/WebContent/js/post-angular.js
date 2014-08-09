64e249a9670247530819622a1e6bdb74b28b07a3
Baraa Bakhsh <bakhshb@gmail.com>
2014-08-09 18:27:05 +1200

    validate, insert data into the api and receive a server response if it is successful or not

--- com.kt.booking.restful/WebContent/js/post-angular.js
+++ com.kt.booking.restful/WebContent/js/post-angular.js
@@ -18,8 +18,17 @@ myApp.controller('postController', function ($scope, Post){
 	
 	$scope.submit = function(){
 		console.log($scope.commitData);
+		//put the data filed into object
 		var postObject = new Object();
 		postObject.first_name = $scope.commitData;
-		Post.create({},postObject);
+		
+		// insert the data into the api and get server response 
+		Post.create({},postObject, function success(data) {
+			if(data[0].HTTP_CODE == 200) {
+				$scope.response= data[0].MSG;
+			}
+			},function err() {
+				$scope.response= 'Please contact the admin';
+			});
 	}
 });
