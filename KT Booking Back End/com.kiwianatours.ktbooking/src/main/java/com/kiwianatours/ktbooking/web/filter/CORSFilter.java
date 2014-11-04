df37593498770f021c3e81f337446b9d11ab5ad5
Baraa Bakhsh <bakhshb@gmail.com>
2014-11-04 18:18:27 +1300

    add comment on cors and override the default health indicator

--- KT Booking Back End/com.kiwianatours.ktbooking/src/main/java/com/kiwianatours/ktbooking/web/filter/CORSFilter.java
+++ KT Booking Back End/com.kiwianatours.ktbooking/src/main/java/com/kiwianatours/ktbooking/web/filter/CORSFilter.java
@@ -14,10 +14,19 @@ public class CorsFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
+		
+		// *(allow from all servers) OR http://crunchify.com/ OR http://example.com/
 		response.setHeader("Access-Control-Allow-Origin", "*");
+		
+		// As a part of the response to a request, which HTTP methods can be used during the actual request.
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+		
+		// How long the results of a request can be cached in a result cache.
 		response.setHeader("Access-Control-Max-Age", "3600");
+		
+		// As part of the response to a request, which HTTP headers can be used during the actual request.
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+		
 		chain.doFilter(req, res);
 	}
 
